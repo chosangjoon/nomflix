4481dbe22421b3e3ee09a2b3ad86ad2a799001bc
cho sang joon <sjbio234@naver.com>
2023-04-10 10:53:36 +0900

    Slider( infinite index)

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { IGetMoviesResult, getMovies } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../Utils";
+import { AnimatePresence, motion } from "framer-motion";
+import useWindowDimensions from "../useWindowDimesions";
 
 const Wrapper = styled.div`
   background-color: black;
+  overflow-x: hidden;
 `;
 const Loader = styled.div`
   height: 20vh;
@@ -18,10 +22,11 @@ const Banner = styled.div<{ bgPhoto: string }>`
   flex-direction: column;
   justify-content: center;
   padding: 60px;
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)),
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
     url(${(props) => props.bgPhoto});
   background-size: cover;
 `;
+
 const Title = styled.h2`
   font-size: 55px;
   margin-bottom: 10px;
@@ -31,6 +36,36 @@ const OverView = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 100px;
+  color: red;
+  font-size: 48px;
+`;
+// const rowVariants = {
+//   hidden: {
+//     x: window.innerWidth + 10,
+//   },
+//   visible: {
+//     x: 0,
+//   },
+//   exit: {
+//     x: -window.innerWidth - 10,
+//   },
+// };
+
 function Home() {
   // useQuery hook을 사용하여 영화 데이터를 가져옴
   // "movies"와 "nowPlaying"을 쿼리 키로 사용하고, getMovies 함수를 통해 데이터를 가져옴
@@ -38,20 +73,55 @@ function Home() {
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => {
+    if (leaving) return;
+    setLeaving(true);
+    setIndex((prev) => prev + 1);
+    else if (index===3){
+      index=0;
+    }
+  };
+
+  const width = useWindowDimensions();
+
+  const [leaving, setLeaving] = useState(false);
+
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
 
   return (
     <>
-      <Wrapper style={{ height: "200vh" }}>
+      <Wrapper>
         {isLoading ? (
           <Loader>Loading...</Loader>
         ) : (
           <>
             <Banner
+              onClick={increaseIndex}
               bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
             >
               <Title>{data?.results[0].title}</Title>
               <OverView>{data?.results[0].overview}</OverView>
             </Banner>
+            <Slider>
+              <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                <Row
+                  // variants={rowVariants}
+                  initial={{ x: width + 10 }}
+                  animate={{ x: 0 }}
+                  exit={{ x: -width - 10 }}
+                  transition={{ type: "tween", duration: 1 }}
+                  key={index}
+                  /* key만 바꿀뿐이지만 react는 새로운 Row컴포넌트로 인식 */
+                >
+                  {[1, 2, 3, 4, 5, 6].map((i) => (
+                    <Box key={i}>{i}</Box>
+                  ))}
+                </Row>
+              </AnimatePresence>
+            </Slider>
           </>
         )}
       </Wrapper>
