fa7c9716c73954dc6f65f84fc0162fee9a692fa1
cho sang joon <sjbio234@naver.com>
2023-04-13 14:35:31 +0900

    URLSearchParams

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 function Search() {
+  const loacation = useLocation();
+  const keyword = new URLSearchParams(loacation.search).get("keyword");
+  console.log(keyword);
   return null;
 }
 export default Search;
